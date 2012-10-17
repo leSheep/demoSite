e2c22bebcd92c8e65f830e61ed2021a01b01dd20
Veganimus <seru1@gmx.de>
2012-10-17 22:00:37 +0300

    Update infos.php

--- infos.php
+++ infos.php
@@ -6,8 +6,27 @@ if(!isset($selectedSite)) {
 ?>
 
 <h1> Infos </h1>
-<p> Infos zu Pelz, Tierrechten, Antispeziesismus und Abolitionismus </p>
-<p> bla bla bla </p>
+<p>Damit Menschen Pelz tragen können, müssen nichtmenschliche Tiere starke Qualen erleiden. Tiere wie Nerze werden in Käfige gesteckt, die wenige Quadratzentimeter groß sind. Sie verbringen ihr ganzes Leben auf diesem drahtigen Flecken. In freier Natur streifen diese Tiere durch Gebiete die mehrere Quadratkilometer groß sind und sie schwimmen in freier Natur sehr gerne in Teichen und Seen. All dies wird ihnen verwehrt. Nach 7 Monaten in den Käfigen, werden sie durch Genickbruch oder durch Vergasung getötet, damit ihnen danach ihr Fell abgezogen werden kann. Und für was? Damit wir unnötige Pelzapplikationen an unseren Jacken haben, damit wir uns „Schick“ fühlen können mit unserem „Echtpelz“. Dabei sind wir schon lange darüber hinweg, Pelz zu benötigen damit es uns Warm ist. 
+</p>
+<p>
+Am Beispiel von Pelz wollen wir auf der Wiesbaden Pelzfrei Demo zeigen, wie nichtmenschliche Tiere in unserer Gesellschaft ausgebeutet werden. Denn egal ob Pelz, Fleisch, Milch, Eier oder andere Tierausbeutungsformen, die Argumente sind immer die selben. Die Unterdrückung von nichtmenschlichen Tieren wird von den Meisten als Natürlich, Normal und Notwendig betrachtet. Doch es ist nicht natürlich oder normal, wenn unsere Gesellschaft die Natur bis aufs letzte ausbeutet, wenn nichtmenschliche Tiere industriell getötet werden, wenn ihr Körper wie ein Zahnrad an die Maschinerie angepasst wird, indem ihnen Schwänze und Schnäbel abgeschnitten werden oder ihre Gene „verbessert“ werden. Vor allem ist Tierausbeutung nicht Notwendig! Wir benötigen keine tierlichen Produkte um uns zu Versorgen. Ganz im Gegenteil! Es beutet die Natur bei weitem mehr aus, als wenn wir die Pflanzen die auf den Feldern wachsen direkt zubereiten würden.
+</p>
+<p>
+Wieso bleibt diese Ideologie der Ausbeutung bisher so lange bestehen? Wir dürfen nicht vergessen dass hinter der Ausbeutung von Tieren große Industrien stehen, welche enorme Profit einfahren. Um Tiere als pure Wertsteigerungsmöglichkeit nutzen zu können, werden diese zu puren Objekten degradiert. Wir werden so davon überzeugt, nichtmenschliche Tiere bloß nach dem Nutzen für uns zu bewerten und vergessen dabei, dass es falsch ist andere Auszubeuten.
+</p>
+<p>
+Wenn wir die Probleme in unserer Welt lösen möchten, muss der Respekt vor Leben gestärkt werden. Die Abhebung des Menschen von anderen Spezies ist eine Ursache auch für innermenschliche Ausbeutung. Die moralische Trennung von Mensch und nichtmenschlichen Tieren hat schon als Basis für viele Unterdrückungen gedient. Frauen werden noch heute eher der „Natur“ zugeschrieben, angeblich emotionaler, gefühlvoller. Männer sind in diesem Dualismus die „Starken“ die Fleisch essen müssen. Genauso wurde die Sklavenhaltung mit der Natürlichkeit gerechtfertigt. Vor jedem Krieg, wird die Bevölkerung des Ziellandes zu Tieren degradiert, als babarisch gebrandmarkt, um so das Töten als weniger schlimm erscheinen zu lassen. Diese Kultur der Ausbeutung muss beendet werden. Deswegen muss Veganismus teil der Lösung sein!
+</p>
+<p>
+Kommt am 26. Januar zur Wiesbaden Pelzfrei Demo und zeigt, dass eine andere, eine vegane Welt möglich ist!
+</p>
+<p>
+Infos über Pelz
+Infos über Naturausbeutung
+Infos über den Zusammenhang von Tierausbeutung und anderen Ausbeutungsformen
+Infos zu Veganismus
+</p>
+
 
 <?php
 }
