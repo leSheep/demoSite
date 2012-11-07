7908ddb0713e7492f0903c044b3d737725078a5c
Veganimus <seru1@gmx.de>
2012-11-07 19:01:57 +0100

    Andere Banner hinzugefügt

--- index.php
+++ index.php
@@ -8,21 +8,33 @@
 </head>
 <body>
 <h1> <img src="banner.png" /> </h1>
-<div class="floating_left border">
+
+<div class="floating_left">
+<div class="menu border">
 <div class="navigationBar">
-    <a href="mainpage.php">STARTSEITE</a>
-	<a href="infos.php">INFOS</a>
-	<a href="staende.php">STÄNDE</a>
-	<a href="abendgestaltung.php">PARTY</a>
-  	<a href="sekten.php">SEKTEN</a>
-  	<a href="anfahrt.php">ANFAHRT</a>
-  	<a href="contact.php">KONTAKT</a>
-  	<a href="downloads.php">DOWNLOADS</a>
+    <a href="mainpage.php">STARTSEITE</a> <br />
+	<a href="infos.php">INFOS</a> <br />
+	<a href="staende.php">STÄNDE</a> <br />
+	<a href="abendgestaltung.php">PARTY</a> <br />
+  	<a href="sekten.php">SEKTEN</a> <br />
+  	<a href="anfahrt.php">ANFAHRT</a> <br />
+  	<a href="contact.php">KONTAKT</a> <br />
+  	<a href="downloads.php">DOWNLOADS</a> <br />
 </div>
 <div class="socialMediaBar">
 	<p>social plugins</p>
 </div></div>
 
+<div style=""><p> weitere Demos/Kampagnen/Aktionen: </p></div>
+<div style="padding-top: 4px;">
+<a href="http://www.maxmaracampaign.net/en/" target="_blank"><img src="http://www.koeln-pelzfrei.de/2009/img/banner_maxmara.gif" alt="" height="50" width="220" border="1"></a>
+</div>
+
+<div style="padding-top: 4px;"><a href="http://offensive-gegen-die-pelzindustrie.net/" target="_blank">
+<img src="http://www.koeln-pelzfrei.de/2007/ogpi_banner.jpg" alt="" height="40" width="220" border="1"></a>
+</div>
+
+</div>
 <div class="index_Main border_top">
 
 <?php
