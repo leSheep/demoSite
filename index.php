d92c0dbb86a3642541bafdfd5f06c496510f9648
leSheep <horror.vacui@web.de>
2012-11-19 22:25:39 +0100

    plugins fertig

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 <!-- Facebook plugin end -->
 
 
-<p style="padding-top:30px;"> <img src="banner.png" /> </p>
+<center><p style="padding-top:30px;padding-bottom:10px;"> <img src="banner.png" /> </p></center>
 
 
 <div class="floating_left">
@@ -38,8 +38,19 @@
 	  		<li><a href="downloads.php">DOWNLOADS</a></li>
 		</ul>
 	</div>
-<div class="socialMediaBar" style="margin-left: 20px;">
+<div class="socialMediaBar" style="margin-left: 20px;margin-top:40px;">
+	<!-- g+ -->
+	<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+	<g:plusone></g:plusone>
+	<!-- twitter -->
+	<a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
+	<script>!function(d,s,id){var 		js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	<!-- linkedIn -->
+	<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+	<script type="IN/Share" data-url="http://wpf.veganseite.de" data-counter="right"></script>
+	<!-- fb -->
 	<div class="fb-like" data-href="http://wpf.veganseite.de/2013/" data-send="false" data-layout="button_count" data-width="200" data-show-faces="false" data-font="arial"></div>
+	
 	
 </div></div>
 
