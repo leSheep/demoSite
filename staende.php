f10b611bdac994d15d0276da53073653300951d8
Veganimus <seru1@gmx.de>
2012-11-21 15:25:08 +0100

    Stände aktualisiert

--- staende.php
+++ staende.php
@@ -11,12 +11,18 @@ if(!isset($selectedSite)) {
 
 <p> Folgende Stände werden dabei sein: </p>
 <ul>
+<li> <a href="http://voilib.de/"> Voice of Liberation </a> </li>
+<li> <a href="http://www.frankfurt-vegan.de/"> Frankfurt Vegan </a> </li>
+<li> <a href="http://www.die-tierfreunde.de/"> Die Tierfreunde </a> </li>
+<li> <a href="http://www.veganlife.lu/"> Vegan Life Shop </a> </li>
+<li> <a href="http://www.tierbefreier.de"> die Tierbefreier </a> </li>
+<li> <a href="http://www.tvg-saar-vegan.de/"> Tierversuchsgegner Saar </a> </li>
+<li> <a href="http://ti-bo.org/"> TiBo - Tierrechtsgruppe Bonn </a> </li>
 <li> <a href=""> </a> </li>
 <li> <a href=""> </a> </li>
 <li> <a href=""> </a> </li>
 <li> <a href=""> </a> </li>
-<li> <a href=""> </a> </li>
-<li> <a href=""> </a> </li>
+
 </ul>
 <?php
 }
