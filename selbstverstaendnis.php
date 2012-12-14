157957377642c0813e148b6a8a48fa3da1de9b74
Veganimus <seru1@gmx.de>
2012-12-14 16:53:07 +0100

    selbstverständnis

--- selbstverstaendnis.php
+++ selbstverstaendnis.php
@@ -6,26 +6,17 @@ if(!isset($selectedSite)) {
 ?>
 
 <h1> Selbstverständnis </h1>
-<p> Wieso wir Sekten wie universelles Leben ablehnen</p>
 
-<ul>
-	<li> emanzipatorisch </li>
-	<ul><li> keine sekten & religiöser kram </li>
-		<li> keine sexisten </li>
-		<li> keine lookisten </li>
-		<li> keine homophoben </li>
-		<li> pazifistisch </li>
-		<li> antiziganismus </li>
-		<li> gegen antisemitismus </li>
-		<li> antipatriotismus </li>
-		<li> antichauvinismus </li>
-		<li> antinationalismus </li>
-		<li> antifaschismus </li>
-	</ul>
-</ul>
+<p> 
+Wir sehen uns als emanzipatorisches Bündnis d.h. wir akzeptieren auf der Demo beispielsweise keine Sekten, Sexisten, Lookisten, Faschisten oder Homophoben. </p>
+
+<p> 
+Wir wünschen uns eine reflektierte Demo, welche auch menschliche Unterdrückungsformen im Auge hat und nicht nach dem Motto "Hauptsache für die Tiere" zulässt, dass beispielsweise mit sexistischen Aktionen etwas "für die Tiere" getan wird. Auch akzeptieren wir aus diesem Grund keine Sekten, welche ihre Mitglieder unterdrücken, auch wenn diese angeblich schon viel "für die Tiere" getan haben sollten.
+</p>
+
+<p> Wir sind neben TierrechtlerInnen auch MenschenrechtlerInnen. Daher finden wir ein emanzipatorisches Auftreten für sich genommen schon sehr wichtig. Allerdings sehen wir noch dazu viele Gemeinsamkeiten in den Erklärungsmustern verschiedener Unterdrückungsformen. Daher glauben wir, dass unemanzipatives Verhalten auch die Ausbeutung von nichtmenschlichen Tieren stabilisieren kann.
 
 <p><b> wir befürworten selbstbestimmtes denken! </b></p>
-
 
 
 <?php
