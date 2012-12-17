1b2635c25f8cb8e04260fb63371d962046b7bd31
Veganimus <seru1@gmx.de>
2012-12-17 12:42:29 +0100

    Downloadseite Banner angepasst

--- downloads.php
+++ downloads.php
@@ -8,10 +8,13 @@ if(!isset($selectedSite)) {
 <h1> Downloads </h1>
 
 <h2 style="text-align:left">Eigener Banner</h2>
-<img src="wpf_banner_small.png" />
 <p> Unseren Banner könnt ihr auf eurer Website einfügen um so zur Demo aufzurufen. Dazu müsst ihr einfach folgenden Code an der gewünschten Stelle einfügen:</p>
 
-<code> &lt;a href=&quot;http://wpf.veganseite.de&quot;&gt; &lt;img src=&quot;http://wpf.veganseite.de/wpf_banner_small.png&quot; /&gt; &lt;/a&gt; </code>
+<img src="wpf_banner_big.png" />
+<p><code> &lt;a href=&quot;http://wpf.tierrechtsdemo.de/&quot;&gt;&lt;img src=&quot;http://wpf.tierrechtsdemo.de/wpf_banner_big.png&quot; width=&quot;468&quot; height=&quot;60&quot; alt=&quot;Wiesbaden Pelzfrei Demo&quot; border=&quot;0&quot; /&gt;&lt;/a&gt; </code></p>
+
+<img src="wpf_banner_small.png" />
+<p><code> &lt;a href=&quot;http://wpf.veganseite.de&quot;&gt; &lt;img src=&quot;http://wpf.veganseite.de/wpf_banner_small.png&quot; width=&quot;234&quot; height=&quot;60&quot; alt=&quot;Wiesbaden Pelzfrei Demo&quot; border=&quot;0&quot; /&gt; &lt;/a&gt; </code></p>
 
 <h2 style="text-align:left;padding-top:25px;"> Unser Flyer </h2>
 
