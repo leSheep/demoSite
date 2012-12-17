c2a6ad99bedc5ca133b863b4e6751f8f42b7d0fe
Veganimus <seru1@gmx.de>
2012-12-17 12:49:39 +0100

    Verbesserungen bei Staende eingepflegt

--- staende.php
+++ staende.php
@@ -7,19 +7,19 @@ if(!isset($selectedSite)) {
 
 <h1>Stände</h1>
 
-<p>Ab 10 Uhr werden am Treffpunkt, dem Vorplatz des Wiesbadener Hauptbahnhofs, verschiedene Stände von Tierrechtsgruppen und Vegan-Shops vertreten sein. Die Stände bleiben bis nach der Demo und dort wird es auch veganes Essen geben.</p>
+<p>Ab 10:30 Uhr werden am Treffpunkt, dem Vorplatz des Wiesbadener Hauptbahnhofs, verschiedene Stände von Tierrechtsgruppen und Vegan-Shops vertreten sein. Die Stände bleiben bis nach der Demo und dort wird es auch veganes Essen geben.</p>
 
 <p> Folgende Stände werden dabei sein: </p>
 <ul>
-<li> <a href="http://voilib.de/"> Voice of Liberation </a> </li>
+<li> <a href="http://www.tierbefreier.de"> die tierbefreier - Ortsgruppe Rhein-Neckar</a> </li>
+<li> <a href="http://www.die-tierfreunde.de/"> die Tierfreunde </a> </li>
 <li> <a href="http://www.frankfurt-vegan.de/"> Frankfurt Vegan </a> </li>
-<li> <a href="http://www.die-tierfreunde.de/"> Die Tierfreunde </a> </li>
+<li> <a href="http://www.tvg-saar-vegan.de/"> Menschen für Tierrechte - Tierversuchsgegner Saar </a> </li>
 <li> <a href="http://www.saveanimals.lu/"> Save Animals </a> </li>
-<li> <a href="http://www.veganlife.lu/"> Vegan Life Shop </a> </li>
-<li> <a href="http://www.tierbefreier.de"> die Tierbefreier </a> </li>
-<li> <a href="http://www.tvg-saar-vegan.de/"> Tierversuchsgegner Saar </a> </li>
+<li> <a href="http://de.seashepherd.org/"> Sea Shepherd </a> </li>
 <li> <a href="http://ti-bo.org/"> TiBo - Tierrechtsgruppe Bonn </a> </li>
-<li> <a href=""> </a> </li>
+<li> <a href="http://www.veganlife.lu/"> vegan life </a> </li>
+<li> <a href="http://voilib.de/"> Voice of Liberation </a> </li>
 <li> <a href=""> </a> </li>
 <li> <a href=""> </a> </li>
 
