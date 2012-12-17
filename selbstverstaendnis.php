a47f80661df3426d770a84b2af19c66961a12fd5
Veganimus <seru1@gmx.de>
2012-12-17 12:55:04 +0100

    Selbstverständnis verbessert

--- selbstverstaendnis.php
+++ selbstverstaendnis.php
@@ -8,15 +8,15 @@ if(!isset($selectedSite)) {
 <h1> Selbstverständnis </h1>
 
 <p> 
-Wir sehen uns als emanzipatorisches Bündnis d.h. wir akzeptieren auf der Demo beispielsweise keine Sekten, Sexisten, Lookisten, Faschisten oder Homophoben. </p>
+Wir sehen uns als emanzipatorisches Bündnis d.h. wir akzeptieren auf der Demo beispielsweise kein Sexismus, Lookismus, Faschismus oder Homophobie und keine Sekten. </p>
 
 <p> 
 Wir wünschen uns eine reflektierte Demo, welche auch menschliche Unterdrückungsformen im Auge hat und nicht nach dem Motto "Hauptsache für die Tiere" zulässt, dass beispielsweise mit sexistischen Aktionen etwas "für die Tiere" getan wird. Auch akzeptieren wir aus diesem Grund keine Sekten, welche ihre Mitglieder unterdrücken, auch wenn diese angeblich schon viel "für die Tiere" getan haben sollten.
 </p>
 
-<p> Wir sind neben TierrechtlerInnen auch MenschenrechtlerInnen. Daher finden wir ein emanzipatorisches Auftreten für sich genommen schon sehr wichtig. Allerdings sehen wir noch dazu viele Gemeinsamkeiten in den Erklärungsmustern verschiedener Unterdrückungsformen. Daher glauben wir, dass unemanzipatives Verhalten auch die Ausbeutung von nichtmenschlichen Tieren stabilisieren kann.
+<p> Wir sind neben Tierrechtler_innen auch Menschenrechtler_innen. Daher finden wir ein emanzipatorisches Auftreten für sich genommen schon sehr wichtig. Allerdings sehen wir noch dazu viele Gemeinsamkeiten in den Erklärungsmustern verschiedener Unterdrückungsformen. Daher glauben wir, dass unemanzipatives Verhalten auch die Ausbeutung von nichtmenschlichen Tieren stabilisieren kann.
 
-<p><b> wir befürworten selbstbestimmtes denken! </b></p>
+<p><b> Wir befürworten selbstbestimmtes Denken! </b></p>
 
 
 <?php
